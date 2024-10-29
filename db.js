c9ed19796b0d32db63c76d480f3fd4869405275d
yujeongFE <109844803+yujeongFE@users.noreply.github.com>
2024-10-29 09:43:27 +0900

    chore: mongo db 연결 성공

--- db.js
+++ db.js
@@ -1,6 +1,16 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
-const MONGO_HOST = process.env.MONGO_HOST;
-mongoose.connect(MONGO_HOST, {
-  retryWrites: true,
-  w: "majority",
-});
+
+const MONGO_HOST = process.env.MONGODB_URI;
+
+if (!MONGO_HOST) {
+  throw new Error("MongoDB URI가 정의되지 않았습니다. .env 파일을 확인하세요.");
+}
+
+mongoose
+  .connect(MONGO_HOST, {
+    retryWrites: true,
+    w: "majority",
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error("MongoDB connection error:", error));
