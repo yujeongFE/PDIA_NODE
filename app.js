87644f9afbb24f9e1b7c1ab04b3bfda257ae54f9
yujeongFE <109844803+yujeongFE@users.noreply.github.com>
2024-10-29 09:26:47 +0900

    기본 요청 응답 보내기

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const path = require("path");
 const logger = require("morgan");
 
 const app = express();
-const indexRouter = require("./routes/index"); // index 라우터 파일 위치
-const userRouter = require("./routes/users"); // user 라우터 파일 위치
+const indexRouter = require("./routes/index");
+const userRouter = require("./routes/users");
+const boardRouter = require("./routes/board");
+const birdsRouter = require("./routes/birds");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -13,14 +15,30 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/hello-world", (req, res) => {
+/* GET home page. */
+app.get("/hello-world", function (req, res) {
   res.json({
-    title: "HelloWorld",
+    title: "Hello World",
     data: "blah blah",
   });
 });
 
+app.post("/hello-world", (req, res) => {
+  res.send("This is Post Request");
+});
+
+app.put("/hello-world", (req, res) => {
+  res.send("This is Put Request");
+});
+
+app.delete("/hello-world", (req, res) => {
+  res.send("This is Delete Request");
+});
+
+// 라우터 사용
 app.use("/", indexRouter);
 app.use("/users", userRouter);
+app.use("/board", boardRouter);
+app.use("/birds", birdsRouter);
 
 module.exports = app;
