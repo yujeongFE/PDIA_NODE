d9095a4bb374c67d3744caf45ebcd9138eadeda6
yujeongFE <109844803+yujeongFE@users.noreply.github.com>
2024-10-30 14:22:54 +0900

    chore: 브라우저에 cookie 저장

--- routes/board.js
+++ routes/board.js
@@ -1,28 +1,42 @@
-let express = require("express");
-let router = express.Router();
+const express = require("express");
+const router = express.Router();
+const Board = require("../models/Board");
 
-let Board = require("../models/Board");
+router.get("/", async (req, res, next) => {
+  try {
+    const boardList = await Board.find();
+    console.log(req.cookies);
 
-router.get("/", function (req, res, next) {
-  Board.find(function (err, boards) {
-    if (err) return next(err);
-    res.json(boards);
-  });
+    res.cookie("cookieName", "cookieValue", {
+      maxAge: 1000 * 60 * 60 * 24,
+      httpOnly: true,
+      secure: false,
+      signed: false,
+    });
+
+    res.json(boardList);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.get("/:id", function (req, res, next) {
-  Board.findById(req.params.id, function (err, board) {
-    if (err) return next(err);
-    if (!board) return next(new Error("Board not found")); // 에러 메시지 추가
+router.get("/:id", async (req, res, next) => {
+  try {
+    const board = await Board.findById(req.params.id);
+    if (!board) {
+      return res.status(404).json({ error: "Board not found" });
+    }
     res.json(board);
-  });
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.post("/", async function (req, res, next) {
+router.post("/", async (req, res, next) => {
   try {
     console.log(req.body);
     const board = await Board.create(req.body);
-    res.json(board);
+    res.status(201).json(board);
   } catch (err) {
     next(err);
   }
